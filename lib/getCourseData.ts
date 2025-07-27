af7e9a45cbbb1a41c028c5b36988fe512713c66c
mezanur-gain <mezanur.rahman@gain.solutions>
2025-07-28 01:18:13 +0600

    feat: update getCourseData to use a fallback API URL for improved reliability

--- lib/getCourseData.ts
+++ lib/getCourseData.ts
@@ -2,7 +2,10 @@ import { API_HEADERS } from "@/lib/constants";
 import { ApiResponse } from "@/types";
 
 export async function getCourseData(lang: string = "en"): Promise<ApiResponse> {
-  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}?lang=${lang}`;
+  const apiBaseUrl =
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://api.10minuteschool.com/discovery-service/api/v1";
+  const apiUrl = `${apiBaseUrl}/products/ielts-course?lang=${lang}`;
   const response = await fetch(apiUrl, {
     headers: API_HEADERS,
     // next: { revalidate: 3600 },
